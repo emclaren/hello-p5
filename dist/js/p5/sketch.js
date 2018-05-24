84b306b879150a72f86c24357f32c7fa17082825
elgin mclaren <elgin_mclaren@hotmail.com>
2018-05-24 14:50:57 -0700

    remove clear function due to bug

--- dist/js/p5/sketch.js
+++ dist/js/p5/sketch.js
@@ -14,50 +14,39 @@ var ydirection = 1; // Top to Bottom
 
 function setup() {
  var canvas = createCanvas(windowWidth, windowWidth/1.85);
+  // var canvas = createCanvas(200, 200);
  canvas.parent('video-overlay');
-  xpos = width / 3;
-  ypos = height / 2;
-     fill(255,0,100);
+ xpos = width /2;
+ ypos = height /2;
+ fill(255,0,100);
 }
 
 
 
 function draw() {
- frameRate(60);
+frameRate(60);
+ // clear();
+ console.log(video.currentTime);
 
-  clear();
-
-console.log(video.currentTime);
-
-if(video.currentTime>1){
-  // Draw the shape
-     ellipseMode(RADIUS);
- 
-    // Update the position of the shape
-  xpos = xpos + xspeed * xdirection;
-  ypos = ypos + yspeed * ydirection;
-
-  // Test to see if the shape exceeds the boundaries of the screen
-  // If it does, reverse its direction by multiplying by -1
+ if(video.currentTime>1){
+  ellipseMode(RADIUS);
+    xpos = xpos + xspeed * xdirection;
+    ypos = ypos + yspeed * ydirection;
   if (xpos > width - rad || xpos < rad) {
     xdirection *= -1;
   }
   if (ypos > height - rad || ypos < rad) {
     ydirection *= -1;
   }
- ellipse(xpos, ypos, rad, rad);
-}
+  ellipse(xpos, ypos, rad, rad);
+  }
 if(video.currentTime>1.5){
-  
   fill(random(255),random(255), random(255));
   }
-if(video.currentTime>3.2){
-// frameRate(0)
 }
 
 
-}
-
+//Resize canvas when window resized. 
 
 function windowResized() {
   resizeCanvas(windowWidth, windowWidth/1.85);
