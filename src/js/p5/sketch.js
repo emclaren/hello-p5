470a4cc55d6a60c07265eb7a866e200fb13a3945
elgin mclaren <elgin_mclaren@hotmail.com>
2018-06-14 13:11:30 -0700

    fixed pixel density issue on desktop

--- src/js/p5/sketch.js
+++ src/js/p5/sketch.js
@@ -2,6 +2,7 @@ var xpos;
 var ypos ;
 
 function setup() {
+ pixelDensity(displayDensity());
  var canvas = createCanvas(windowWidth, windowWidth/1.85); //make this mobile friendly
  canvas.parent('video-overlay');
  xpos = windowWidth/1.3;
@@ -10,7 +11,11 @@ function setup() {
 
 }
 
+
+
 function draw() {
+
+
 
 	//First sketch, starts playing at 2 seconds
 	if(sketch=="sketch1"){
@@ -24,18 +29,22 @@ function draw() {
 	}
 	//Second sketch, starts playing at 5 seconds
 	else if(sketch =="sketch2"){
+		clear();
 		strokeWeight(5);
 		fill(255,0,0);
 		//   ellipseMode(RADIUS);
 		stroke(123,0,255);
 		ellipse(xpos, ypos, 50, 60);
 		xpos=xpos-2;
-	
+		
 	}
 else{
 console.log("no sketch loaded yet");
 }
 
+}
+function mousePressed() {
+  clear();
 }
 
 function windowResized() {
