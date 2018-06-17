e89aa36292a25982fb59809dafdd4f6c650c46be
elgin mclaren <elgin_mclaren@hotmail.com>
2018-06-17 10:55:19 -0700

    remove canvas working

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,12 @@
 //Target html elements so the class of hide can be added on play
 var header = document.querySelector('.header');
 var container = document.querySelector('.container');
+var myp5 = "hello";
+var myp52;
+// var sketch ;  //I currently have a variable named sketch that changes the contents of the draw function in p5. Is this the best strategy? or should I be using namespacing to create multiple sketches and then importing them. 
 
-var sketch ; //I currently have a variable named sketch that changes the contents of the draw function in p5. Is this the best strategy? or should I be using namespacing to create multiple sketches and then importing them. 
+
+
 
 //plyr bare minimum setup code
 document.addEventListener('DOMContentLoaded', () => { 
@@ -13,100 +17,44 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 //plyr triggered when video starts
-  player.on('playing', event => {
+player.on('playing', event => {
     header.classList.add('hide'); //shrink the header on large screens 
     container.classList.add('hide'); //Move the video so it is spaced correctly on large screens
+
   });
 
-//Watch time in video and trigger events
-  player.on('timeupdate', event => {
-    const instance = event.detail.plyr;
-    if(instance.currentTime>2 && instance.currentTime<5){
-      console.log("more than three, less than 5");
-      sketch = "sketch1";
-    }
+// //Watch time in video and trigger events
+player.on('timeupdate', event => {
+  const instance = event.detail.plyr;
+  if(instance.currentTime>2 && instance.currentTime<5){
+
+    console.log("more than three, less than 5");
+     var  myp5 = new p5(sketch);
+     function removeCanvas(){
+  myp5.remove();
+  console.log("remove canvas")
+
+};
+
+
+setTimeout(removeCanvas, 1000);
+
+}
+
     if(instance.currentTime>5){
       console.log("more than 5");
-      sketch = "sketch2";
+      myp52 = new p5(sketch2);
     }
+//     if(instance.currentTime>7){
+//      console.log("more than 5");
+//       var myp53 = new p5(sketch3);
+//     }
   });
 });
 
 
 
+// var  myp5 = new p5(sketch);
 
-
-
-
-
-
-
-
-// //timeupdate demo - logs time of video being played
-// var video = document.getElementById('player');
-// if(typeof(window.MediaController) === 'function')
-// {
-//   var controller = new MediaController();
-//   video.controller = controller;
-
-// } 
-// else
-// {
-//   controller = null;
-// }
-
-// video.addEventListener('play', function() 
-// {
-
-//   console.log("Video started playing");
-
-// }, false);
-
-// video.addEventListener('pause', function()
-// {
-//    // header.classList.remove('hide');
-//    container.classList.remove('hide');
-//  console.log("Video paused ");
-// }, false);
-
-// video.addEventListener('playing', function()
-// {
-//     header.classList.add('hide');
-//     container.classList.add('hide');
-
-//  console.log("testing playing");
-// }, false);
-// video.addEventListener('ended', function()
-// {
-
-//  if(controller)
-//  {
-
-//   controller.pause();
-// }
-// else
-// {
-//   video.pause();
-
-// }
-// }, false);
-
-// video.addEventListener('timeupdate', function()
-// {
-//   // console.log(video.currentTime)
-
-//   // if (video.currentTime>1){
-//   //   console.log("GREATER THAN 2");
-//   //  ypos=mouseY;
-//   //  xpos=mouseX;
-
-//   // }
-//   // else if(video.currentTime<1){
-//   //   console.log("greater than 1");
-
-
-//   // }
-
-// }, false);
 
 
