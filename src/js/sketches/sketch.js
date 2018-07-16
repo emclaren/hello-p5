049903f2bec3447d76826852afb11a3d5a2eaa2f
emclaren <SIAT@SIATs-MacBook-Pro.local>
2018-07-16 07:35:59 -0700

    local browserify test working'

--- src/js/sketches/sketch.js
+++ src/js/sketches/sketch.js
@@ -1,24 +1,7 @@
- 
-function(p){
-	let windowWidth2 = window.innerWidth;
-	p.xpos=100;
-	p.ypos=100;
-	p.setup = function(){
-		this._pixelDensity = 1;
-		background(200,200,200);
-		var canvas = p.createCanvas(500, 500/1.85); //make this mobile friendly
-		canvas.parent('video-overlay');
-		p.fill(0,133,255);
-			console.log("setup")
-	}
+module.exports = function() {
+	 return ['Barry Allen', 'Hal Jordan', 'Kara Kent', 'Diana Prince', 'Ray Palmer', 'Oliver Queen', 'Bruce Wayne', 'Wally West', 'John Jones', 'Kyle Rayner', 'Arthur Curry', 'Clark Kent'];
 
-	p.draw = function(){
-		p.strokeWeight(5);
-		p.stroke(255,255,255);
-		p.ellipse(p.xpos, p.ypos, 100, 100);
-		p.textSize(32);
-		console.log("drawing")
-	}
-	
 }
+
+
 
