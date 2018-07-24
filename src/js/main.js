57d5e3c755bd4e5ac972776af0e54c1dc25e4cbe
emclaren <SIAT@SIATs-MacBook-Pro.local>
2018-07-24 10:36:20 -0700

    fixed time change bug

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 const Plyr = require('plyr'); //works
+
 const p5 = require('p5'); //works; 
 const p5dom = require('../../node_modules/p5/lib/addons/p5.dom');//works
 const sketchfile = require('./sketches/sketch1.js');
@@ -7,16 +8,16 @@ const sketchfile3 = require('./sketches/sketch3.js');
 
 
 
-//For adding the CSS class "hide" to the header when the video playback starts 
+
+
+//For targeting html elements o the class of hide can be added on play
 let header = document.querySelector('.header');
 let container = document.querySelector('.container');
-
 
 // Sketches - TODO : CLEANUP SKETCHES
 let p5test
 let myp52
 let myp53
-
 
 // For Keeping track of current video time
 let videoCurrentTime;
@@ -25,10 +26,7 @@ let videoPreviousTime;
 
 
 
-
-
-
-//Setup code for the plyr video player
+//plyr bare minimum setup code
 document.addEventListener('DOMContentLoaded', () => { 
   const player = new Plyr('#player');
   function on(selector, type, callback) {
@@ -38,8 +36,61 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-var videoTimeAdjusted = false;
+
+
+///Toggle Language Based on Header Link
+var languageArray
+document.getElementById('language-link-spanish').onclick=function(){
+   languageArray= ["Descargar","Empezar","Referencia","Bibliotecas","Aprender","Comunidad"]
+   languageToggle()   
+   player.captions.language="es";
+   removeCaptions();
+
+};
+
+
+document.getElementById('language-link-french').onclick=function(){
+  languageArray= ["Télécharger","Start-fr","Commencer","Bibliothèques","Apprendre","Communauté"]
+  languageToggle()
+  player.captions.language="es"
+  removeCaptions();
+};
+
+
+document.getElementById('language-link-english').onclick=function(){
+  languageArray=["Download", "Start", "Reference", "Libraries", "Learn", "Community"]
+  languageToggle()
+  player.captions.language="en"
+  removeCaptions();
+};
+
+
+function removeCaptions(){
+   var  parentElement = document.getElementsByClassName('plyr__captions')[0];
+   while (parentElement.hasChildNodes()) {
+       parentElement.removeChild(parentElement.firstChild);
+   }
+}
+
+function languageToggle(){
+  document.getElementById("nav-download").innerHTML = languageArray[0];
+  document.getElementById("nav-start").innerHTML = languageArray[1];
+  document.getElementById("nav-reference").innerHTML = languageArray[2]
+  document.getElementById("nav-libraries").innerHTML = languageArray[3]
+  document.getElementById("nav-learn").innerHTML = languageArray[4]
+  document.getElementById("nav-community").innerHTML = languageArray[5]
+}
+
+
+
+
+
+
+var skipped = false;
 var sceneChange =[0,  7,  12, 13, 15,17,19];
+
+
+
 
 
 // Triggered when video start
@@ -47,6 +98,7 @@ player.on('playing', event => {
     header.classList.add('hide'); //shrink the header on large screens 
     container.classList.add('hide'); //Move the video so it is spaced correctly on large screens
 });
+
 
 
 // Triggered when video paused
@@ -61,12 +113,15 @@ player.on('seeking', event => {
 if(p5test){
    p5test.remove(); //remove old p5 sketch when user selects a different time in the video
 }
-  var videoTimeAdjusted = true;
-
+   skipped = true;
+    // p5test.frameRate(0);
 });
 
 player.on('seeked', event => {
-    p5test.frameRate(0); // makes p5 sketch pause when 
+   //this is what's causing the issue when you move. 
+
+  
+    p5test.frameRate(0);
 });
 
 
@@ -79,9 +134,9 @@ player.on('seeked', event => {
     videoPreviousTime=videoCurrentTime;
     videoCurrentTime=Math.round(instance.currentTime)
 
-    // console.log(skipped);
-    if(videoTimeAdjusted){
-         videoTimeAdjusted=false
+    console.log(skipped);
+    if(skipped){
+         skipped=false
         for(var i=0; i< sceneChange.length;i++){
             if(videoCurrentTime>=sceneChange[i]  && videoCurrentTime<sceneChange[i+1]){
             console.log("scene change " + sceneChange[i]);
@@ -100,11 +155,9 @@ player.on('seeked', event => {
         if(p5test){
             p5test.frameRate(60);
         }
-        console.log("video current time" + videoCurrentTime);
-        console.log("video previous time " + videoPreviousTime);
-    
-            videoTimeAdjusted=false;
-        
+        console.log("video current time in switch " + videoCurrentTime);
+        console.log("video previous time in switch " + videoPreviousTime);
+        skipped=false;
       
         console.log("videoCurrentTime is different than videoPreviousTime");
 
@@ -158,53 +211,6 @@ player.on('seeked', event => {
 
 });
 });
-
-
-
-
-///Toggle Language Based on Header Link
-var languageArray
-document.getElementById('language-link-spanish').onclick=function(){
-   languageArray= ["Descargar","Empezar","Referencia","Bibliotecas","Aprender","Comunidad"]
-   languageToggle()   
-   player.captions.language="es";
-   removeCaptions();
-
-};
-
-
-document.getElementById('language-link-french').onclick=function(){
-  languageArray= ["Télécharger","Start-fr","Commencer","Bibliothèques","Apprendre","Communauté"]
-  languageToggle()
-  player.captions.language="es"
-  removeCaptions();
-};
-
-
-document.getElementById('language-link-english').onclick=function(){
-  languageArray=["Download", "Start", "Reference", "Libraries", "Learn", "Community"]
-  languageToggle()
-  player.captions.language="en"
-  removeCaptions();
-};
-
-
-function removeCaptions(){
-   var  parentElement = document.getElementsByClassName('plyr__captions')[0];
-   while (parentElement.hasChildNodes()) {
-       parentElement.removeChild(parentElement.firstChild);
-   }
-}
-
-function languageToggle(){
-  document.getElementById("nav-download").innerHTML = languageArray[0];
-  document.getElementById("nav-start").innerHTML = languageArray[1];
-  document.getElementById("nav-reference").innerHTML = languageArray[2]
-  document.getElementById("nav-libraries").innerHTML = languageArray[3]
-  document.getElementById("nav-learn").innerHTML = languageArray[4]
-  document.getElementById("nav-community").innerHTML = languageArray[5]
-}
-
 
 
 
