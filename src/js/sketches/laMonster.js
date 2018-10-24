c017b7ab8cffc17699a18fe7ce5571f455b1500f
Elgin-Skye McLaren <elginskye@Dot-Matrix.attlocal.net>
2018-10-23 23:45:06 -0400

    clean up sketches

--- src/js/sketches/laMonster.js
+++ src/js/sketches/laMonster.js
@@ -1,92 +1,82 @@
 const p5play = require('./play.js');
 
-
 var laMonster= function(p5){
-	let monsterlooBackground;
-	let monsterloo;
-	let fastmove = false;
-	let standmonster1;
-	let standmonster2;
-	let standmonster3;
-	let moveright = false;
-	let moveleft = true;
-	let magicX = 300;
-	let monster;
-	let collisionTest = false;
-	let a =0 ; 
-	let resize=true;
+	let monsterlooBackground; // background image
+	let fastmove = false; // sprite moves faster depending on mouse position
+	let standmonster; //pink monster standing in background
+	let moveright = false; //determine direction of the sprite
+	let monster; // game spirte
+	let fade = 0 ;  // variable to fade in/out the sketch
+	let magicX; // speed the character
+	
+	
 	p5.preload = () => {
-		monsterlooBackground = p5.loadImage("https://emclaren.github.io/hello-p5/dist/assets/img/MonsterLoo_Background2.png"); 
-	}
-
-
-
+		monsterlooBackground = p5.loadImage("dist/assets/img/MonsterLoo_Background.png"); // preload the background image
+	};
+	
 	p5.setup = () => {
 		p5.pixelDensity(1);
 		p5.windowWidth = document.documentElement.clientWidth;
 		p5.windowHeight = p5.windowWidth  * 0.562;
 		p5.canvas= p5.createCanvas(p5.windowWidth/2.8, p5.windowHeight/2);
 		p5.canvas.parent('video-overlay');
-		p5.canvas.addClass('la-monster-sketch');
-		monster = p5.createSprite( p5.width/2, p5.height/1.25, 40, 40);
-		monster.addAnimation("floating", "dist/assets/img/BS_1.png", "dist/assets/img/BS_5.png");
-		monster.addAnimation("right", "dist/assets/img/BR_1.png", "dist/assets/img/BR_9.png");
-		monster.depth = 5;
-		monster.scale = p5.width/2000;
-		standmonster1 = p5.createSprite(p5.width*1.8, p5.height/1.2);  
-		standmonster1.addAnimation("normal", "https://emclaren.github.io/hello-p5/dist/assets/img/EM_2.png");
-		standmonster1.setCollider("rectangle", 0,0, 170,280);
-		standmonster1.debug = collisionTest;
-		standmonster1.depth = 3;
-		standmonster1.scale = p5.width/2000;
-		let magicX = p5.width/3;
+		p5.canvas.addClass('la-monster-sketch'); // Add class to add margin to the canvas
+		monster = p5.createSprite( p5.width/2, p5.height/1.25, 40, 40); // location of monster sprite
+		monster.addAnimation("floating", "dist/assets/img/laMonsterLeftStanding_1.png", "dist/assets/img/laMonsterLeftStanding_5.png"); 
+		monster.addAnimation("right", "dist/assets/img/laMonsterLeftMove_1.png", "dist/assets/img/laMonsterLeftMove_9.png");
+		monster.depth = 5; // the z-index of the characters
+		monster.scale = p5.width/2000; //reduce the size of the monster
+		standmonster = p5.createSprite(p5.width*1.8, p5.height/1.2);  // pink monster in the background location
+		standmonster.addAnimation("normal", "dist/assets/img/laMonsterBackgroundSprite.png"); //pink monster iamage
+		standmonster.depth = 3; // the z-index of the characters
+		standmonster.scale = p5.width/2000; //reduce the size of the monster
+		magicX = p5.width/3; // initial character position
 		p5.noStroke();
-	}
+	};
 	
 	p5.draw = () => {
-		if(a<255){
-		a+=10;
+		// fade in the sketch using an overlapping rectangle
+		if(fade < 255){
+			fade += 10;
 		}
+		// fade out the sketch
 		if(window.videoCurrentTimeGlobal > 2.25){
-			a-=30;
-		}
-		p5.image(monsterlooBackground, -p5.width/3, 0, p5.height*6, p5.height); 
+			fade -= 30;
+		}	
+		p5.fill(255,255,255,255-fade);
+		p5.rect(0, 0, p5.width * 5, p5.height);
 
-		if(window.videoCurrentTimeGlobal>2.75){
-			p5.clear()
-		}
-		p5.monsterdraw();
-		p5.cameraview();
+
+		p5.image(monsterlooBackground, -p5.width/3, 0, p5.height*6, p5.height); // position the background
+		p5.monsterdraw(); 
+		p5.cameraview(); 
 		p5.drawSprites();
 		p5.camera.on();
-		p5.fill(255,255,255,255-a);
-		p5.rect(0, 0, p5.width * 5, p5.height)
-	}
+
+	};
 	
-
-
 	p5.cameraview = () => {	
 		p5.camera.position.x = magicX;
 		if ( p5.mouseX > 200 && p5.mouseX < 800 || p5.mouseX == 0){
 			fastmove = false;
 			if( p5.camera.position.x < monster.position.x) {
 				moveright = true;
-				magicX += 3;
+				magicX += 3; // move sprite
 			}
 		} 
 		else if ( p5.mouseX > 800){
 			fastmove = true;
-				if( p5.camera.position.x <monster.position.x) {
-					magicX += 5;
+			if( p5.camera.position.x <monster.position.x) {
+				magicX += 5; //move sprite
 			}
 		} 
 		else if ( p5.mouseX < 200){
 			fastmove = false;
-				if( p5.camera.position.x > p5.mouseX){
-					magicX -= 3;
-				}	
+			if( p5.camera.position.x > p5.mouseX){
+				magicX -= 3; //move sprite
+			}	
 		} 
-	}
+	};
 	
 	p5.monsterdraw = () => {	
 		// -------- creating Monster -------		
@@ -100,7 +90,7 @@ var laMonster= function(p5){
 				monster.velocity.x = 2.3;
 			}	
 		}
-			//if mouse is to the left
+		//if mouse is to the left
 		else if(p5.camera.mouseX < monster.position.x - 10) {
 			monster.changeAnimation("right");
 			//flip horizontally
@@ -121,8 +111,8 @@ var laMonster= function(p5){
 			monster.changeAnimation("floating");
 			monster.velocity.x = 0;
 		}
-	}	
-}
+	};
+};
 
 
 
